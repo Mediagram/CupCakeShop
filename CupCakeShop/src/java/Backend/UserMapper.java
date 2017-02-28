42344704b77abd40d401c7e5afe4873d3937c39b
Bade <benjaminlrasmussen@gmail.com>
2017-02-28 16:06:06 +0100

    Usermapper proto

--- CupCakeShop/src/java/Backend/UserMapper.java
+++ CupCakeShop/src/java/Backend/UserMapper.java
@@ -7,6 +7,7 @@ package Backend;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,10 +21,28 @@ public class UserMapper
 {
     public User loginUser(String email, String password)
     {
-//        password = HashEncoder.get_SHA_256_SecurePassword(password);
+        // password = HashEncoder.get_SHA_256_SecurePassword(password, null);
         String sql = "select * from User where email = ? and password = ?";
+        Connection con = new DBConnector().getConnection();
+        User user = null;
         
-        return null;
+        try
+        {
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            
+            if (rs.next())
+            {
+                user = new User(rs.getInt("id"), rs.getString("email"),
+                rs.getString("password"), rs.getString("name"));
+            }
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(UserMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return user;
     }
     
     public void createUser(User newUser)
@@ -31,12 +50,15 @@ public class UserMapper
         String sql = "insert into User (email, password, name) values (?,?,?)";
         Connection con = new DBConnector().getConnection();
         
+        //String password = HashEncoder.get_SHA_256_SecurePassword(newUser.getPassword(), salt);
+        
         try
             (PreparedStatement stmt = con.prepareStatement(sql);)
         {
             stmt.setString(1, newUser.getEmail());
             stmt.setString(2, newUser.getPassword());
             stmt.setString(3, newUser.getName());
+            stmt.executeUpdate();
         } catch (SQLException ex)
         {
             Logger.getLogger(UserMapper.class.getName()).log(Level.SEVERE, null, ex);
--- CupCakeShop/src/java/model/User.java
+++ CupCakeShop/src/java/model/User.java
@@ -8,6 +8,14 @@ public class User
     private String password;
     private String name;
 
+    public User(int id, String email, String password, String name)
+    {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+    
     public int getId()
     {
         return id;
